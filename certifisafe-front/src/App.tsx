a9a3c97f89e012e7a9ec4951646a2aba66a9659b
coma007 <milicasladakovic7@gmail.com>
2023-03-21 00:02:49 +0100

    Created banner

--- certifisafe-front/src/App.tsx
+++ certifisafe-front/src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css';
+import Banner from 'components/Banner/Banner';
+import AppCSS from './App.module.css';
 
 function App() {
-  return <div></div>
+  return <div className={AppCSS.main}><Banner></Banner></div>
 }
 
 export default App;
