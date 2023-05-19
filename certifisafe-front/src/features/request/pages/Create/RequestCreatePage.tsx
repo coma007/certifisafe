cfbf502a3f1be6910276d20f98a7f832ed2598be
coma007 <milicasladakovic7@gmail.com>
2023-05-19 21:47:37 +0200

    Finishing touches

--- certifisafe-front/src/features/request/pages/Create/RequestCreatePage.tsx
+++ certifisafe-front/src/features/request/pages/Create/RequestCreatePage.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import ModalWindow from 'components/view/Modal/ModalWindow'
 import ModalWindowCSS from 'components/view/Modal/ModalWindow.module.scss'
+import RequestCreatePageCSS from './RequestCreatePage.module.scss'
 import { useState } from 'react'
 import InputField from 'components/forms/InputField/InputField'
 
 const RequestCreatePage = (props: { createIsOpen: boolean, closeCreateModal: any }) => {
     return (
         <ModalWindow
-            height="75%"
+            height="87%"
             isOpen={props.createIsOpen}
             closeWithdrawalModal={props.closeCreateModal}
             title="Create new certificate"
@@ -17,33 +18,45 @@ const RequestCreatePage = (props: { createIsOpen: boolean, closeCreateModal: any
             <InputField usage="Signing certificate serial number" className={ModalWindowCSS.input} />
             <InputField usage="Certificate name" className={ModalWindowCSS.input} />
             <div>
-                <input
-                    type="radio"
-                    name="radioGroup"
-                    id="foryou"
-                // checked={selectedOption === "foryou"}
-                // onChange={handleOptionChange}
-                />
-                <label htmlFor="foryou">Root</label>
-                <input
-                    type="radio"
-                    name="radioGroup"
-                    id="fromyou"
-                // checked={selectedOption === "fromyou"}
-                // onChange={handleOptionChange}
-                />
-                <label htmlFor="fromyou">Intermediate</label>
-                <input
-                    type="radio"
-                    name="radioGroup"
-                    id="fromyou"
-                // checked={selectedOption === "fromyou"}
-                // onChange={handleOptionChange}
-                />
-                <label htmlFor="fromyou">End</label>
+                <p>
+                    Type of certificate:
+                    <div className={RequestCreatePageCSS.option}>
+                        <input
+                            type="radio"
+                            name="radioGroup"
+                            id="foryou"
+                        // checked={selectedOption === "root"}
+                        // onChange={handleOptionChange}
+                        />
+                        <label htmlFor="foryou">Root</label>
+                        <span className={RequestCreatePageCSS.description}>Signed by our root certificate and can sign other certificates.</span>
+                    </div>
+                    <div className={RequestCreatePageCSS.option}>
+                        <input
+                            type="radio"
+                            name="radioGroup"
+                            id="fromyou"
+                        // checked={selectedOption === "intermediate"}
+                        // onChange={handleOptionChange}
+                        />
+                        <label htmlFor="fromyou">Intermediate</label>
+                        <span className={RequestCreatePageCSS.description}>Must be signed by other certificate and can sign other certificates.</span>
+                    </div>
+                    <div className={RequestCreatePageCSS.option}>
+                        <input
+                            type="radio"
+                            name="radioGroup"
+                            id="fromyou"
+                        // checked={selectedOption === "end"}
+                        // onChange={handleOptionChange}
+                        />
+                        <label htmlFor="fromyou">End</label>
+                        <span className={RequestCreatePageCSS.description}>Must be signed by other certificate and cannot sign other certificates.</span>
+                    </div>
+                </p>
             </div>
             <p className={ModalWindowCSS.warning}>Please note that this certificate will be active if the request is accepted by the signing certificate owner. If so, certificate will be valid from that day until the expiration of the signing certificate.</p>
-        </ModalWindow>
+        </ModalWindow >
     )
 }
 
