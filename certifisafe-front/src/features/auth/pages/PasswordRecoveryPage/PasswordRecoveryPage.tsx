dc81ca83da82a8ff64754cb67b21da10128f11b4
coma007 <milicasladakovic7@gmail.com>
2023-05-20 23:24:10 +0200

    Finished password recovery - new password page

--- certifisafe-front/src/features/auth/pages/PasswordRecoveryPage/PasswordRecoveryPage.tsx
+++ certifisafe-front/src/features/auth/pages/PasswordRecoveryPage/PasswordRecoveryPage.tsx
@@ -1,11 +1,29 @@
+import Button from 'components/forms/Button/Button';
+import InputField from 'components/forms/InputField/InputField';
+import Banner from 'components/navigation/Banner/Banner';
+import PasswordRecoveryForm from 'features/auth/components/PasswordRecoveryForm/PasswordRecoveryForm';
 import React, { useState } from 'react'
 import VerificationInput from "react-verification-input";
 
 const PasswordRecoveryPage = () => {
 
   return (
-    <div>
-      <VerificationInput />
+
+    <div className="page pageTwoCols">
+      <div>
+        <Banner />
+      </div>
+      <div className="rightCol">
+        <div className="authTitle">
+          <h2>Reset password</h2>
+          <span >
+            We have sent you an email with verification code.
+            <br />
+            Please enter the code bellow and new password.
+          </span>
+        </div>
+        <PasswordRecoveryForm />
+      </div >
     </div>
   )
 }
