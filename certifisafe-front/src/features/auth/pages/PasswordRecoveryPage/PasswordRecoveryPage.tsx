ffe28fd3dc108a553bbc3b47eb3c0d647b11a594
coma007 <milicasladakovic7@gmail.com>
2023-05-20 16:45:37 +0200

    Added Verification Input

---
+++ certifisafe-front/src/features/auth/pages/PasswordRecoveryPage/PasswordRecoveryPage.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import VerificationInput from "react-verification-input";
+
+const PasswordRecoveryPage = () => {
+
+  return (
+    <div>
+      <VerificationInput />
+    </div>
+  )
+}
+
+export default PasswordRecoveryPage
