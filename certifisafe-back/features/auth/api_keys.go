12c4105eb7f135b178b8c3e6220904dff1a5ab01
SV 10/2020 <nemanja.majstorovic3214@gmail.com>
2023-06-17 01:21:52 +0200

    Removed keys

--- certifisafe-back/features/auth/api_keys.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-const TwillioApiUsername = "ACfa3e5d6f88377babb803e52047931303"
-const TwillioApiPassword = "b94ed9430a56e4b7f04cb578b30ddb7c"
-const MailgunApiDomain = "sandbox8a19eac035a044aba758c74091673332.mailgun.org"
-const MailgunApiKey = "ea6de29817328d924b4d747db570d895-af778b4b-7a65e75d"
-const MailgunApiBase = "https://api.mailgun.net/v3"
